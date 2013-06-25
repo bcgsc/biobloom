6f7355dac1e6dcc2929e462b69330b2d4bf62622
cjustin <cjustin@yqs-centos.phage.bcgsc.ca>
2013-06-25 15:36:04 -0700

    forgot to add

---
+++ Common/Dynamicofstream.cpp
@@ -0,0 +1,57 @@
+/*
+ * Dynamicofstream.cpp
+ *
+ *  Created on: Jun 19, 2013
+ *      Author: cjustin
+ */
+
+#include "Dynamicofstream.h"
+#include "gzstream.h"
+#include "StringUtil.h"
+#include <iostream>
+#include <fstream>
+
+Dynamicofstream::Dynamicofstream(const string &filename)
+{
+	if(endsWith(filename, ".gz"))
+	{
+		filestream = new ogzstream(filename.c_str(), ios::out);
+		gz = true;
+	}
+	else{
+		filestream = new ofstream(filename.c_str(), ios::out);
+		gz = false;
+	}
+}
+
+ostream& Dynamicofstream::operator <<(const string& o)
+{
+	*filestream << o;
+	return *filestream;
+}
+
+ostream& Dynamicofstream::operator <<(uint16_t o)
+{
+	*filestream << o;
+	return *filestream;
+}
+
+
+void Dynamicofstream::close(){
+	filestream->flush();
+	if (gz){
+		ogzstream *temp = dynamic_cast<ogzstream*>(filestream);
+		temp->close();
+	}
+	else{
+		ofstream *temp = dynamic_cast<ofstream*>(filestream);
+		temp->close();
+	}
+}
+
+Dynamicofstream::~Dynamicofstream()
+{
+	close();
+	delete filestream;
+}
+
