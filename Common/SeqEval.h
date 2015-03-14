cfc991a352e9554158988c737e210ed754a1090f
JustinChu <cjustin@bcgsc.ca>
2015-03-13 17:36:30 -0700

    fixes

--- Common/SeqEval.h
+++ Common/SeqEval.h
@@ -22,9 +22,9 @@ using namespace boost;
 
 namespace SeqEval {
 /*
- * Evaluation algorithm with no hashValue storage
+ * Evaluation algorithm with no hashValue storage (optimize speed for single queries)
  */
-inline bool evalSingle(FastqRecord rec, unsigned kmerSize, BloomFilter filter,
+inline bool evalSingle(const FastqRecord &rec, unsigned kmerSize, const BloomFilter &filter,
 		double threshold, unsigned antiThreshold)
 {
 	ReadsProcessor proc(kmerSize);
@@ -39,6 +39,9 @@ inline bool evalSingle(FastqRecord rec, unsigned kmerSize, BloomFilter filter,
 				if (filter.contains(currentKmer)) {
 					score += 0.5;
 					++streak;
+					if (threshold <= score) {
+						return true;
+					}
 				}
 				else if (antiThreshold <= ++antiScore) {
 					return false;
@@ -90,73 +93,80 @@ inline bool evalSingle(FastqRecord rec, unsigned kmerSize, BloomFilter filter,
 	return false;
 }
 
-///*
-// * Evaluation algorithm with hashValue storage
-// */
-//inline bool evalSingle(FastqRecord rec, unsigned kmerSize, BloomFilter filter,
-//		double threshold, double antiThreshold, unsigned hashNum,
-//		vector<vector<size_t> > &hashValues)
-//{
-//	ReadsProcessor proc(kmerSize);
-//	size_t currentLoc = 0;
-//	double score = 0;
-//	unsigned antiScore = 0;
-//	unsigned streak = 0;
-//	while (rec.seq.length() >= currentLoc + kmerSize) {
-//		const unsigned char* currentKmer = proc.prepSeq(rec.seq, currentLoc);
-//		if (streak == 0) {
-//			if (currentKmer != NULL) {
-//				if (filter.contains(currentKmer)) {
-//					score += 0.5;
-//					++streak;
-//				}
-//				else if (antiThreshold <= ++antiScore) {
-//					return false;
-//				}
-//				++currentLoc;
-//			} else {
-//				if (currentLoc > kmerSize) {
-//					currentLoc += kmerSize + 1;
-//					antiScore += kmerSize + 1;
-//				} else {
-//					++antiScore;
-//					++currentLoc;
-//				}
-//				if (antiThreshold <= antiScore) {
-//					return false;
-//				}
-//			}
-//		} else {
-//			if (currentKmer != NULL) {
-//				if (filter.contains(currentKmer)) {
-//					++streak;
-//					score += 1 - 1 / (2 * streak);
-//					++currentLoc;
-//
-//					if (threshold <= score) {
-//						return true;
-//					}
-//					continue;
-//				}
-//				else if (antiThreshold <= ++antiScore) {
-//					return false;
-//				}
-//			} else {
-//				currentLoc += kmerSize + 1;
-//				antiScore += kmerSize + 1;
-//			}
-//			if (streak < opt::streakThreshold) {
-//				++currentLoc;
-//			} else {
-//				currentLoc += kmerSize;
-//				antiScore += kmerSize;
-//			}
-//			if (antiThreshold <= antiScore) {
-//				return false;
-//			}
-//			streak = 0;
-//		}
-//}
+/*
+ * Evaluation algorithm with hashValue storage (minimize redundant work)
+ */
+inline bool evalSingle(const FastqRecord &rec, unsigned kmerSize, const BloomFilter &filter,
+		double threshold, double antiThreshold, unsigned hashNum,
+		vector<vector<size_t> > &hashValues)
+{
+	ReadsProcessor proc(kmerSize);
+	size_t currentLoc = 0;
+	double score = 0;
+	unsigned antiScore = 0;
+	unsigned streak = 0;
+	while (rec.seq.length() >= currentLoc + kmerSize) {
+		const unsigned char* currentSeq = proc.prepSeq(rec.seq, currentLoc);
+		if (streak == 0) {
+			if (currentSeq != NULL) {
+				hashValues[currentLoc] = multiHash(currentSeq, hashNum, kmerSize);
+				if (filter.contains(hashValues[currentLoc])) {
+					score += 0.5;
+					++streak;
+					if (threshold <= score) {
+						return true;
+					}
+				}
+				else if (antiThreshold <= ++antiScore) {
+					return false;
+				}
+				++currentLoc;
+			} else {
+				if (currentLoc > kmerSize) {
+					currentLoc += kmerSize + 1;
+					antiScore += kmerSize + 1;
+				} else {
+					++antiScore;
+					++currentLoc;
+				}
+				if (antiThreshold <= antiScore) {
+					return false;
+				}
+			}
+		} else {
+			if (currentSeq != NULL) {
+				hashValues[currentLoc] = multiHash(currentSeq, hashNum, kmerSize);
+				if (filter.contains(hashValues[currentLoc])) {
+					++streak;
+					score += 1 - 1 / (2 * streak);
+					++currentLoc;
+
+					if (threshold <= score) {
+						return true;
+					}
+					continue;
+				}
+				else if (antiThreshold <= ++antiScore) {
+					return false;
+				}
+			} else {
+				currentLoc += kmerSize + 1;
+				antiScore += kmerSize + 1;
+			}
+			if (streak < opt::streakThreshold) {
+				++currentLoc;
+			} else {
+				currentLoc += kmerSize;
+				antiScore += kmerSize;
+			}
+			if (antiThreshold <= antiScore) {
+				return false;
+			}
+			streak = 0;
+		}
+	}
+	return false;
+}
 }
 ;
 
