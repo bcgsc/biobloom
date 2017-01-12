7531751f719277ed1ef3bbca839a5ba72d97488d
JustinChu <cjustin@bcgsc.ca>
2017-01-12 14:10:58 -0800

    new progressive options work without subtractive filter

--- BioBloomMaker/BloomFilterGenerator.cpp
+++ BioBloomMaker/BloomFilterGenerator.cpp
@@ -224,7 +224,7 @@ size_t BloomFilterGenerator::generateProgressive(const string &filename,
 					sequence2.breakClose();
 					cerr << "K-mer threshold reached at read "
 							<< totalReads << " iteration "
-							<< i << endl;
+							<< i+1 << endl;
 				}
 			}
 
@@ -421,151 +421,172 @@ size_t BloomFilterGenerator::generateProgressive(const string &filename,
 	for(unsigned i = 0; i < opt::threads; ++i){
 		procs[i] = boost::shared_ptr<ReadsProcessor>(new ReadsProcessor(m_kmerSize));
 	}
-
+	for (unsigned i = 0; i < opt::progItrns; ++i) {
+		size_t totalReads = 0;
+		cerr << "Iteration " << i + 1 << endl;
 #pragma omp parallel
-	for (FastqRecord rec1;;) {
-		FastqRecord rec2;
-		bool good1;
-		bool good2;
+		for (FastqRecord rec1;;) {
+			FastqRecord rec2;
+			bool good1;
+			bool good2;
 
-		if (m_totalEntries >= m_expectedEntries) {
-			//so threshold message only printed once
-			if (sequence1.eof() || sequence2.eof()) {
-				break;
-			}
+			if (m_totalEntries >= m_expectedEntries) {
+				//so threshold message only printed once
+				if (sequence1.eof() || sequence2.eof()) {
+					break;
+				}
 #pragma omp critical(breakClose)
-			{
-				sequence1.breakClose();
-				sequence2.breakClose();
-				cerr << "K-mer threshold reached at read " << totalReads << endl;
-			}
-		}
-
-#pragma omp critical(sequence)
-		{
-			good1 = sequence1 >> rec1;
-			good2 = sequence2 >> rec2;
-		}
-
-		if (good1 && good2) {
-#pragma omp critical(totalReads)
-			{
-				++totalReads;
-				if (totalReads % 10000000 == 0) {
-					cerr << "Currently Reading Read Number: " << totalReads
+				{
+					sequence1.breakClose();
+					sequence2.breakClose();
+					cerr << "K-mer threshold reached at read " << totalReads
 							<< endl;
 				}
 			}
-			string tempStr1 = rec1.id.substr(0, rec1.id.find_last_of("/"));
-			string tempStr2 = rec2.id.substr(0, rec2.id.find_last_of("/"));
-			if (tempStr1 == tempStr2) {
-				size_t numKmers1 = rec1.seq.length() > m_kmerSize ? rec1.seq.length() - m_kmerSize + 1 : 0;
-				size_t numKmers2 = rec2.seq.length() > m_kmerSize ? rec2.seq.length() - m_kmerSize + 1 : 0;
+
+#pragma omp critical(sequence)
+			{
+				good1 = sequence1 >> rec1;
+				good2 = sequence2 >> rec2;
+			}
+
+			if (good1 && good2) {
+#pragma omp critical(totalReads)
+				{
+					++totalReads;
+					if (totalReads % 10000000 == 0) {
+						cerr << "Currently Reading Read Number: " << totalReads
+								<< endl;
+					}
+				}
+				string tempStr1 = rec1.id.substr(0, rec1.id.find_last_of("/"));
+				string tempStr2 = rec2.id.substr(0, rec2.id.find_last_of("/"));
+				if (tempStr1 == tempStr2) {
+					size_t numKmers1 =
+							rec1.seq.length() > m_kmerSize ?
+									rec1.seq.length() - m_kmerSize + 1 : 0;
+					size_t numKmers2 =
+							rec2.seq.length() > m_kmerSize ?
+									rec2.seq.length() - m_kmerSize + 1 : 0;
 //				if(numKmers2 == 0){
 //					cerr << rec1.seq.length() << endl;;
 //					cerr << rec2.seq.length() << endl;;
 //					exit(1);
 //				}
-				vector<vector<size_t> > hashValues1(numKmers1);
-				vector<vector<size_t> > hashValues2(numKmers2);
-				switch (mode) {
-				case PROG_INC: {
-					if (numKmers1 > score && SeqEval::evalRead(rec1, m_kmerSize, filter,
-							score, 1.0 - score, m_hashNum,
-							hashValues1, evalMode)) {
-						//load remaining sequences
-						for (unsigned i = 0; i < numKmers1; ++i) {
-							if (hashValues1[i].empty()) {
-								const unsigned char* currentSeq = procs[omp_get_thread_num()]->prepSeq(
-										rec1.seq, i);
-								insertKmer(currentSeq, filter);
-							} else {
-								insertKmer(hashValues1[i], filter);
+					vector<vector<size_t> > hashValues1(numKmers1);
+					vector<vector<size_t> > hashValues2(numKmers2);
+					switch (mode) {
+					case PROG_INC: {
+						if (numKmers1 > score
+								&& SeqEval::evalRead(rec1, m_kmerSize, filter,
+										score, 1.0 - score, m_hashNum,
+										hashValues1, evalMode)) {
+							//load remaining sequences
+							for (unsigned i = 0; i < numKmers1; ++i) {
+								if (hashValues1[i].empty()) {
+									const unsigned char* currentSeq =
+											procs[omp_get_thread_num()]->prepSeq(
+													rec1.seq, i);
+									insertKmer(currentSeq, filter);
+								} else {
+									insertKmer(hashValues1[i], filter);
+								}
 							}
-						}
-						//load store second read
-						for (unsigned i = 0; i < numKmers2; ++i) {
-							const unsigned char* currentSeq = procs[omp_get_thread_num()]->prepSeq(
-									rec2.seq, i);
-							insertKmer(currentSeq, filter);
-						}
-						if (printReads)
-							printReadPair(rec1, rec2);
-					} else if (numKmers2 > score && SeqEval::evalRead(rec2, m_kmerSize, filter,
-							score, 1.0 - score, m_hashNum, hashValues2, evalMode)) {
-						//load remaining sequences
-						for (unsigned i = 0; i < numKmers1; ++i) {
-							if (hashValues1[i].empty()) {
-								const unsigned char* currentSeq = procs[omp_get_thread_num()]->prepSeq(
-										rec1.seq, i);
+							//load store second read
+							for (unsigned i = 0; i < numKmers2; ++i) {
+								const unsigned char* currentSeq =
+										procs[omp_get_thread_num()]->prepSeq(
+												rec2.seq, i);
 								insertKmer(currentSeq, filter);
-							} else {
-								insertKmer(hashValues1[i], filter);
 							}
-						}
-						//load store second read
-						for (unsigned i = 0; i < numKmers2; ++i) {
-							if (hashValues2[i].empty()) {
-								const unsigned char* currentSeq = procs[omp_get_thread_num()]->prepSeq(
-										rec2.seq, i);
-								insertKmer(currentSeq, filter);
-							} else {
-								insertKmer(hashValues2[i], filter);
+							if (printReads)
+								printReadPair(rec1, rec2);
+						} else if (numKmers2 > score
+								&& SeqEval::evalRead(rec2, m_kmerSize, filter,
+										score, 1.0 - score, m_hashNum,
+										hashValues2, evalMode)) {
+							//load remaining sequences
+							for (unsigned i = 0; i < numKmers1; ++i) {
+								if (hashValues1[i].empty()) {
+									const unsigned char* currentSeq =
+											procs[omp_get_thread_num()]->prepSeq(
+													rec1.seq, i);
+									insertKmer(currentSeq, filter);
+								} else {
+									insertKmer(hashValues1[i], filter);
+								}
 							}
+							//load store second read
+							for (unsigned i = 0; i < numKmers2; ++i) {
+								if (hashValues2[i].empty()) {
+									const unsigned char* currentSeq =
+											procs[omp_get_thread_num()]->prepSeq(
+													rec2.seq, i);
+									insertKmer(currentSeq, filter);
+								} else {
+									insertKmer(hashValues2[i], filter);
+								}
+							}
+							if (printReads)
+								printReadPair(rec1, rec2);
 						}
-						if (printReads)
-							printReadPair(rec1, rec2);
+						break;
 					}
-					break;
-				}
-				case PROG_STD: {
-					try{
-					if (numKmers1 > score && SeqEval::evalRead(rec1, m_kmerSize, filter,
-							score, 1.0 - score, m_hashNum, hashValues1, evalMode)
-							&& numKmers2 > score && SeqEval::evalRead(rec2, m_kmerSize, filter,
-								score, 1.0 - score, m_hashNum, hashValues2, evalMode)) {
-						//load remaining sequences
-						for (unsigned i = 0; i < numKmers1; ++i) {
-							if (hashValues1[i].empty()) {
-								const unsigned char* currentSeq = procs[omp_get_thread_num()]->prepSeq(
-										rec1.seq, i);
-								insertKmer(currentSeq, filter);
-							} else {
-								insertKmer(hashValues1[i], filter);
+					case PROG_STD: {
+						try {
+							if (numKmers1 > score
+									&& SeqEval::evalRead(rec1, m_kmerSize,
+											filter, score, 1.0 - score,
+											m_hashNum, hashValues1, evalMode)
+									&& numKmers2 > score
+									&& SeqEval::evalRead(rec2, m_kmerSize,
+											filter, score, 1.0 - score,
+											m_hashNum, hashValues2, evalMode)) {
+								//load remaining sequences
+								for (unsigned i = 0; i < numKmers1; ++i) {
+									if (hashValues1[i].empty()) {
+										const unsigned char* currentSeq =
+												procs[omp_get_thread_num()]->prepSeq(
+														rec1.seq, i);
+										insertKmer(currentSeq, filter);
+									} else {
+										insertKmer(hashValues1[i], filter);
+									}
+								}
+								//load store second read
+								for (unsigned i = 0; i < numKmers2; ++i) {
+									if (hashValues2[i].empty()) {
+										const unsigned char* currentSeq =
+												procs[omp_get_thread_num()]->prepSeq(
+														rec2.seq, i);
+										insertKmer(currentSeq, filter);
+									} else {
+										insertKmer(hashValues2[i], filter);
+									}
+								}
+								if (printReads)
+									printReadPair(rec1, rec2);
 							}
+						} catch (const std::bad_alloc&) {
+							cerr << "2 " << totalReads << endl;
+							exit(6);
 						}
-						//load store second read
-						for (unsigned i = 0; i < numKmers2; ++i) {
-							if (hashValues2[i].empty()) {
-								const unsigned char* currentSeq = procs[omp_get_thread_num()]->prepSeq(
-										rec2.seq, i);
-								insertKmer(currentSeq, filter);
-							} else {
-								insertKmer(hashValues2[i], filter);
-							}
-						}
-						if (printReads)
-							printReadPair(rec1, rec2);
+						break;
 					}
-					} catch (const std::bad_alloc&) {
-						cerr << "2 " << totalReads << endl;
-						exit(6);
 					}
-					break;
+				} else {
+					cerr << "Read IDs do not match" << "\n" << tempStr1 << "\n"
+							<< tempStr2 << endl;
+					exit(1);
 				}
-				}
-			} else {
-				cerr << "Read IDs do not match" << "\n" << tempStr1 << "\n"
-						<< tempStr2 << endl;
-				exit(1);
-			}
-		} else
-			break;
-	}
-	if (!sequence1.eof() || !sequence2.eof()) {
-		cerr
-				<< "error: eof bit not flipped. Input files may be different lengths"
-				<< endl;
+			} else
+				break;
+		}
+		if (!sequence1.eof() || !sequence2.eof()) {
+			cerr
+					<< "error: eof bit not flipped. Input files may be different lengths"
+					<< endl;
+		}
 	}
 
 	filter.storeFilter(filename);
