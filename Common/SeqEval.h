a966318f23e192a42524a9baaeda49f0374ef6df
JustinChu <cjustin@bcgsc.ca>
2017-05-29 12:00:48 -0700

    remove code that is not doing anything

--- Common/SeqEval.h
+++ Common/SeqEval.h
@@ -92,7 +92,7 @@ inline bool evalSingle(const string &rec, unsigned kmerSize, const BloomFilter &
 				if ((subtract == NULL || !subtract->contains(hash))
 						&& filter.contains(hash)) {
 					++streak;
-					score += 1 - 1 / (2 * streak);
+					++score;
 					++currentLoc;
 
 					if (threshold <= score) {
@@ -278,7 +278,7 @@ inline double evalSingleExhaust(const string &rec, unsigned kmerSize,
 			if (currentKmer != NULL) {
 				if (filter.contains(currentKmer)) {
 					++streak;
-					score += 1 - 1 / (2 * streak);
+					++score;
 					++currentLoc;
 					continue;
 				}
@@ -295,78 +295,6 @@ inline double evalSingleExhaust(const string &rec, unsigned kmerSize,
 	}
 	return score;
 }
-
-///*
-// * Evaluation algorithm with no hashValue storage (optimize speed for single queries)
-// * Returns length when stopping threshold is met
-// * Returns the length of the read where threshold was met
-// */
-//inline unsigned evalSingleLength(const string &rec, unsigned kmerSize,
-//		const BloomFilter &filter, double threshold, double antiThreshold)
-//{
-//	ReadsProcessor proc(kmerSize);
-//	size_t currentLoc = 0;
-//	double score = 0;
-//	unsigned antiScore = 0;
-//	unsigned streak = 0;
-//	while (rec.length() >= currentLoc + kmerSize) {
-//		const unsigned char* currentKmer = proc.prepSeq(rec, currentLoc);
-//		if (streak == 0) {
-//			if (currentKmer != NULL) {
-//				if (filter.contains(currentKmer)) {
-//					score += 0.5;
-//					++streak;
-//					if (threshold <= score) {
-//						return currentLoc;
-//					}
-//				} else if (antiThreshold <= ++antiScore) {
-//					return rec.length() - kmerSize;
-//				}
-//				++currentLoc;
-//			} else {
-//				if (currentLoc > kmerSize) {
-//					currentLoc += kmerSize + 1;
-//					antiScore += kmerSize + 1;
-//				} else {
-//					++antiScore;
-//					++currentLoc;
-//				}
-//				if (antiThreshold <= antiScore) {
-//					return rec.length() - kmerSize;
-//				}
-//			}
-//		} else {
-//			if (currentKmer != NULL) {
-//				if (filter.contains(currentKmer)) {
-//					++streak;
-//					score += 1 - 1 / (2 * streak);
-//
-//					if (threshold <= score) {
-//						return currentLoc;
-//					}
-//					++currentLoc;
-//					continue;
-//				} else if (antiThreshold <= ++antiScore) {
-//					return rec.length() - kmerSize;
-//				}
-//			} else {
-//				currentLoc += kmerSize + 1;
-//				antiScore += kmerSize + 1;
-//			}
-//			if (streak < opt::streakThreshold) {
-//				++currentLoc;
-//			} else {
-//				currentLoc += kmerSize;
-//				antiScore += kmerSize;
-//			}
-//			if (antiThreshold <= antiScore) {
-//				return rec.length() - kmerSize;
-//			}
-//			streak = 0;
-//		}
-//	}
-//	return rec.length() - kmerSize;
-//}
 
 /*
  * Core evaluation algorithm, with ability start evaluating sequence midway
@@ -438,7 +366,7 @@ inline bool eval(const string &rec, unsigned kmerSize,
 			if (hashValues[currentLoc].size() > 0) {
 				if (filter.contains(hashValues[currentLoc])) {
 					++streak;
-					score += 1 - 1 / (2 * streak);
+					++score;
 					++currentLoc;
 
 					if (threshold <= score) {
