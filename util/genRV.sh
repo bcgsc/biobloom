f897738e77db0d22de7a3201eb49b95f93a5f605
JustinChu <cjustin@bcgsc.ca>
2018-04-05 16:00:42 -0700

    improve rc script to accept glob statement

--- util/genRV.sh
+++ util/genRV.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 #Placeholder script for generating .rv (reversed files) needed for indexing a present time
 #Requires gnu awk, tac, tr, and seqtk to be in your path
 #usage genRV.sh filename.fa
-cat $1 | awk '/^>/ {printf("%s%s\t",(N>0?"\n":""),$0);N++;next;} {printf("%s",$0);} END {printf("\n");}' | tac | tr "\t" "\n" |seqtk seq -r - > $1.rv
-echo $1".rv written"
+for filename; do
+	cat $filename | awk '/^>/ {printf("%s%s\t",(N>0?"\n":""),$0);N++;next;} {printf("%s",$0);} END {printf("\n");}' | tac | tr "\t" "\n" |seqtk seq -r - > $filename.rv
+	echo $filename".rv written"
+done
