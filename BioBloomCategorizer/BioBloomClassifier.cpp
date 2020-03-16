69d7569294da46978a1ed42dece1cae4706699b3
JustinChu <cjustin@bcgsc.ca>
2020-03-16 16:53:36 -0700

    adjust score binomal output to phred scale

--- BioBloomCategorizer/BioBloomClassifier.cpp
+++ BioBloomCategorizer/BioBloomClassifier.cpp
@@ -1198,7 +1198,7 @@ double BioBloomClassifier::evaluateReadBestHit(const string &rec,
 	double maxScore = 0;
 
 	for (unsigned i = 0; i < m_filters.size(); ++i) {
-		double score = SeqEval::evalScore(rec, *m_filters[i]);
+		double score = log10(SeqEval::evalScore(rec, *m_filters[i]))*-10;
 		if (maxScore < score) {
 			maxScore = score;
 			bestFilters.clear();
