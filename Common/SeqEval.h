027fa1afe7437df5de33ee762467a0b06b66c59a
cjustin <cjustin@bcgsc.ca>
2020-07-30 02:40:31 -0700

    Add dusting for all seqEval functions

--- Common/SeqEval.h
+++ Common/SeqEval.h
@@ -19,7 +19,7 @@
 #include "btl_bloomfilter/BloomFilter.hpp"
 #include "btl_bloomfilter/vendor/ntHashIterator.hpp"
 #include <boost/math/distributions/binomial.hpp>
-#include "Common/sdust.h"
+#include "Common/SDust.hpp"
 
 using namespace std;
 
@@ -36,7 +36,7 @@ inline double normalizeScore(double score, unsigned kmerSize, size_t seqLen) {
 
 inline bool evalSimple(const string &rec, const BloomFilter &filter,
 		double threshold, const BloomFilter *subtract =
-		NULL) {
+		NULL, SDust *sduster = NULL) {
 
 	const double thres = denormalizeScore(threshold, filter.getKmerSize(),
 			rec.length());
@@ -50,7 +50,8 @@ inline bool evalSimple(const string &rec, const BloomFilter &filter,
 	ntHashIterator itr(rec, filter.getKmerSize(), filter.getKmerSize());
 	unsigned prevPos = 0;
 	if (itr != itr.end()) {
-		if (filter.contains(*itr)) {
+		if (!(sduster != NULL && sduster->isLowComp(itr.pos()))
+				&& filter.contains(*itr)) {
 			if (subtract == NULL || !subtract->contains(*itr))
 				score += 0.5;
 			if (thres <= score) {
@@ -74,7 +75,8 @@ inline bool evalSimple(const string &rec, const BloomFilter &filter,
 			}
 			streak = 0;
 		}
-		if (filter.contains(*itr)) {
+		if (!(sduster != NULL && sduster->isLowComp(itr.pos()))
+				&& filter.contains(*itr)) {
 			if (streak == 0) {
 				if (subtract == NULL || !subtract->contains(*itr))
 					score += 0.5;
@@ -113,7 +115,7 @@ inline bool evalSimple(const string &rec, const BloomFilter &filter,
 
 inline bool evalHarmonic(const string &rec, const BloomFilter &filter,
 		double threshold, const BloomFilter *subtract =
-		NULL) {
+		NULL, SDust *sduster = NULL) {
 
 	const double thres = denormalizeScore(threshold, filter.getKmerSize(),
 			rec.length());
@@ -127,7 +129,8 @@ inline bool evalHarmonic(const string &rec, const BloomFilter &filter,
 	ntHashIterator itr(rec, filter.getKmerSize(), filter.getKmerSize());
 	unsigned prevPos = 0;
 	if (itr != itr.end()) {
-		if (filter.contains(*itr)) {
+		if (!(sduster != NULL && sduster->isLowComp(itr.pos()))
+				&& filter.contains(*itr)) {
 			if (subtract == NULL || !subtract->contains(*itr))
 				score += 0.5;
 			if (thres <= score) {
@@ -151,7 +154,8 @@ inline bool evalHarmonic(const string &rec, const BloomFilter &filter,
 			}
 			streak = 0;
 		}
-		if (filter.contains(*itr)) {
+		if (!(sduster != NULL && sduster->isLowComp(itr.pos()))
+				&& filter.contains(*itr)) {
 			if (streak == 0) {
 				if (subtract == NULL || !subtract->contains(*itr))
 					score += 0.5;
@@ -213,8 +217,8 @@ inline double calcProbMatches(size_t frameLen, double bfFPR, size_t matches) {
 
 inline bool evalBinomial(const string &rec, const BloomFilter &filter,
 		double threshold, const BloomFilter *subtract =
-		NULL) {
-	if(rec.size() <  filter.getKmerSize()) {
+		NULL, SDust *sduster = NULL) {
+	if (rec.size() < filter.getKmerSize()) {
 		return false;
 	}
 	const unsigned frameLen = rec.size() - filter.getKmerSize() + 1;
@@ -228,7 +232,8 @@ inline bool evalBinomial(const string &rec, const BloomFilter &filter,
 	ntHashIterator itr(rec, filter.getKmerSize(), filter.getKmerSize());
 	unsigned prevPos = 0;
 	if (itr != itr.end()) {
-		if (filter.contains(*itr)) {
+		if (!(sduster != NULL && sduster->isLowComp(itr.pos()))
+				&& filter.contains(*itr)) {
 			if (subtract == NULL || !subtract->contains(*itr))
 				score++;
 			if (thres <= score) {
@@ -252,7 +257,8 @@ inline bool evalBinomial(const string &rec, const BloomFilter &filter,
 			}
 			streak = 0;
 		}
-		if (filter.contains(*itr)) {
+		if (!(sduster != NULL && sduster->isLowComp(itr.pos()))
+				&& filter.contains(*itr)) {
 			if (subtract == NULL || !subtract->contains(*itr))
 					++score;
 			if (thres <= score) {
@@ -287,7 +293,8 @@ inline bool evalBinomial(const string &rec, const BloomFilter &filter,
  * Evaluation algorithm based on minimum number of contiguous matching bases.
  */
 inline bool evalMinMatchLen(const string &rec, const BloomFilter &filter,
-		unsigned minMatchLen, const BloomFilter *subtract = NULL) {
+		unsigned minMatchLen, const BloomFilter *subtract = NULL,
+		SDust *sduster = NULL) {
 	// number of contiguous k-mers matched
 	unsigned matchLen = 0;
 	size_t l = rec.length();
@@ -304,7 +311,8 @@ inline bool evalMinMatchLen(const string &rec, const BloomFilter &filter,
 		if (itr.pos() != prevPos + 1) {
 			matchLen = 0;
 		}
-		if (filter.contains(*itr)) {
+		if (!(sduster != NULL && sduster->isLowComp(itr.pos()))
+				&& filter.contains(*itr)) {
 			if (subtract == NULL || !subtract->contains(*itr)) {
 				if (matchLen == 0)
 					matchLen = filter.getKmerSize();
@@ -323,31 +331,8 @@ inline bool evalMinMatchLen(const string &rec, const BloomFilter &filter,
 	return false;
 }
 
-inline bool evalRead(const string &rec, const BloomFilter &filter,
-		double threshold, const BloomFilter *subtract = NULL) {
-	switch (opt::scoringMethod) {
-	case opt::LENGTH:
-		return evalMinMatchLen(rec, filter, (unsigned) round(threshold),
-				subtract);
-	case opt::HARMONIC:
-		return evalHarmonic(rec, filter, threshold, subtract);
-	case opt::BINOMIAL:
-		return evalBinomial(rec, filter, threshold, subtract);
-	case opt::SIMPLE:
-	default:
-		return evalSimple(rec, filter, threshold, subtract);
-	}
-	assert(false);
-}
-
-inline bool evalRead(const string &rec, const BloomFilter &filter,
-		double threshold, const BloomFilter &subtract) {
-	return evalRead(rec, filter, threshold, &subtract);
-}
-
 inline double evalSimpleScore(const string &rec, const BloomFilter &filter,
-		const BloomFilter *subtract =
-		NULL) {
+		const BloomFilter *subtract = NULL, SDust *sduster = NULL) {
 
 	double score = 0;
 	unsigned streak = 0;
@@ -359,7 +344,8 @@ inline double evalSimpleScore(const string &rec, const BloomFilter &filter,
 		if (itr.pos() != prevPos + 1) {
 			streak = 0;
 		}
-		if (filter.contains(*itr)) {
+		if (!(sduster != NULL && sduster->isLowComp(itr.pos()))
+				&& filter.contains(*itr)) {
 			if (streak == 0) {
 				if (subtract == NULL || !subtract->contains(*itr))
 					score += 0.5;
@@ -390,8 +376,7 @@ inline double evalSimpleScore(const string &rec, const BloomFilter &filter,
 }
 
 inline double evalHarmonicScore(const string &rec, const BloomFilter &filter,
-		const BloomFilter *subtract =
-		NULL) {
+		const BloomFilter *subtract = NULL, SDust *sduster = NULL) {
 	double score = 0;
 	unsigned streak = 0;
 	ntHashIterator itr(rec, filter.getKmerSize(), filter.getKmerSize());
@@ -402,7 +387,8 @@ inline double evalHarmonicScore(const string &rec, const BloomFilter &filter,
 		if (itr.pos() != prevPos + 1) {
 			streak = 0;
 		}
-		if (filter.contains(*itr)) {
+		if (!(sduster != NULL && sduster->isLowComp(itr.pos()))
+				&& filter.contains(*itr)) {
 			if (streak == 0) {
 				if (subtract == NULL || !subtract->contains(*itr))
 					score += 0.5;
@@ -433,7 +419,8 @@ inline double evalHarmonicScore(const string &rec, const BloomFilter &filter,
 }
 
 inline unsigned evalMinMatchLenScore(const string &rec,
-		const BloomFilter &filter, const BloomFilter *subtract = NULL) {
+		const BloomFilter &filter, const BloomFilter *subtract = NULL,
+		SDust *sduster = NULL) {
 	unsigned matchLen = 0;
 	ntHashIterator itr(rec, filter.getHashNum(), filter.getKmerSize());
 	unsigned prevPos = 0;
@@ -443,7 +430,8 @@ inline unsigned evalMinMatchLenScore(const string &rec,
 		if (itr.pos() != prevPos + 1) {
 			matchLen = 0;
 		}
-		if (filter.contains(*itr)) {
+		if ((!(sduster != NULL && sduster->isLowComp(itr.pos()))
+				&& filter.contains(*itr))) {
 			if (subtract == NULL || !subtract->contains(*itr)) {
 				if (matchLen == 0)
 					matchLen = filter.getKmerSize();
@@ -460,8 +448,7 @@ inline unsigned evalMinMatchLenScore(const string &rec,
 }
 
 inline double evalBinomialScore(const string &rec, const BloomFilter &filter,
-		const BloomFilter *subtract =
-		NULL) {
+		const BloomFilter *subtract = NULL, SDust *sduster = NULL) {
 	if (rec.size() < filter.getKmerSize()) {
 		return 1.0;
 	}
@@ -471,96 +458,108 @@ inline double evalBinomialScore(const string &rec, const BloomFilter &filter,
 	ntHashIterator itr(rec, filter.getKmerSize(), filter.getKmerSize());
 	unsigned prevPos = 0;
 
-	//TODO: this is a prototype dusting operation. Can be better optimized by digging into sdust function
-	if (opt::dust) {
-		uint64_t *r;
-		int n;
-		r = sdust(0, (uint8_t*)rec.c_str(), -1, opt::dustK, opt::dustWindow, &n);
-		int i = 0;
-
-		while (itr != itr.end()) {
-			//check if k-mer has deviated/started again
-			//TODO try to terminate before itr has to re-init after skipping
-			if (itr.pos() != prevPos + 1) {
-				streak = 0;
-			}
-			while (i < n && itr.pos() >= (unsigned) r[i]) {
-				++i;
-			}
-			if (!(i < n && itr.pos() >= (unsigned) (r[i] >> 32)
-					&& itr.pos() < (unsigned) r[i]) && filter.contains(*itr)) {
-				if (subtract == NULL || !subtract->contains(*itr))
-					++score;
-				prevPos = itr.pos();
-				++itr;
-				++streak;
-			} else {
-				if (streak < opt::streakThreshold) {
-					prevPos = itr.pos();
-					++itr;
-				} else {
-					unsigned skipEnd = itr.pos() + filter.getKmerSize();
-					//skip lookups
-					//TODO Add skip functionality to nthash (faster skip or reconstruct)
-					while (itr.pos() < skipEnd) {
-						prevPos = itr.pos();
-						++itr;
-					}
-				}
-				streak = 0;
-			}
+	while (itr != itr.end()) {
+		//check if k-mer has deviated/started again
+		//TODO try to terminate before itr has to re-init after skipping
+		if (itr.pos() != prevPos + 1) {
+			streak = 0;
 		}
-		free(r);
-	} else {
-		while (itr != itr.end()) {
-			//check if k-mer has deviated/started again
-			//TODO try to terminate before itr has to re-init after skipping
-			if (itr.pos() != prevPos + 1) {
-				streak = 0;
-			}
-			if (filter.contains(*itr)) {
-				if (subtract == NULL || !subtract->contains(*itr))
-					++score;
+		if (!(sduster != NULL && sduster->isLowComp(itr.pos()))
+				&& filter.contains(*itr)) {
+			if (subtract == NULL || !subtract->contains(*itr))
+				++score;
+			prevPos = itr.pos();
+			++itr;
+			++streak;
+		} else {
+			if (streak < opt::streakThreshold) {
 				prevPos = itr.pos();
 				++itr;
-				++streak;
 			} else {
-				if (streak < opt::streakThreshold) {
+				unsigned skipEnd = itr.pos() + filter.getKmerSize();
+				//skip lookups
+				//TODO Add skip functionality to nthash (faster skip or reconstruct)
+				while (itr.pos() < skipEnd) {
 					prevPos = itr.pos();
 					++itr;
-				} else {
-					unsigned skipEnd = itr.pos() + filter.getKmerSize();
-					//skip lookups
-					//TODO Add skip functionality to nthash (faster skip or reconstruct)
-					while (itr.pos() < skipEnd) {
-						prevPos = itr.pos();
-						++itr;
-					}
 				}
-				streak = 0;
 			}
+			streak = 0;
 		}
 	}
 	return calcProbMatches(frameLen, filter.getFPRPrecompute(), score);
 }
 
+inline bool evalRead(const string &rec, const BloomFilter &filter,
+		double threshold, const BloomFilter *subtract = NULL) {
+
+	if(opt::dust){
+		SDust sduster(rec);
+		switch (opt::scoringMethod) {
+		case opt::LENGTH:
+			return evalMinMatchLen(rec, filter, (unsigned) round(threshold), subtract, &sduster);
+		case opt::HARMONIC:
+			return evalHarmonic(rec, filter, threshold, subtract, &sduster);
+		case opt::BINOMIAL:
+			return evalBinomial(rec, filter, threshold, subtract, &sduster);
+		case opt::SIMPLE:
+		default:
+			return evalSimple(rec, filter, threshold, subtract, &sduster);
+		}
+	}
+	else{
+		switch (opt::scoringMethod) {
+		case opt::LENGTH:
+			return evalMinMatchLen(rec, filter, (unsigned) round(threshold),
+					subtract);
+		case opt::HARMONIC:
+			return evalHarmonic(rec, filter, threshold, subtract);
+		case opt::BINOMIAL:
+			return evalBinomial(rec, filter, threshold, subtract);
+		case opt::SIMPLE:
+		default:
+			return evalSimple(rec, filter, threshold, subtract);
+		}
+	}
+}
+
+inline bool evalRead(const string &rec, const BloomFilter &filter,
+		double threshold, const BloomFilter &subtract) {
+	return evalRead(rec, filter, threshold, &subtract);
+}
+
 /*
- * Evaluation algorithm with no hashValue storage (optimize speed for single queries)
- * Returns score instead if just a match
+ * Computes exhaustively (no ending early to save speed)
  */
 inline double evalScore(const string &rec, const BloomFilter &filter, const BloomFilter *subtract =
 		NULL) {
 
-	switch (opt::scoringMethod) {
-	case opt::LENGTH:
-		return evalMinMatchLenScore(rec, filter, subtract);
-	case opt::HARMONIC:
-		return evalHarmonicScore(rec, filter, subtract);
-	case opt::BINOMIAL:
-		return log10(evalBinomialScore(rec, filter, subtract)) * -10;
-	case opt::SIMPLE:
-	default:
-		return evalSimpleScore(rec, filter, subtract);
+	if(opt::dust){
+		SDust sduster(rec);
+		switch (opt::scoringMethod) {
+		case opt::LENGTH:
+			return evalMinMatchLenScore(rec, filter, subtract, &sduster);
+		case opt::HARMONIC:
+			return evalHarmonicScore(rec, filter, subtract, &sduster);
+		case opt::BINOMIAL:
+			return log10(evalBinomialScore(rec, filter, subtract, &sduster)) * -10;
+		case opt::SIMPLE:
+		default:
+			return evalSimpleScore(rec, filter, subtract, &sduster);
+		}
+	}
+	else{
+		switch (opt::scoringMethod) {
+		case opt::LENGTH:
+			return evalMinMatchLenScore(rec, filter, subtract);
+		case opt::HARMONIC:
+			return evalHarmonicScore(rec, filter, subtract);
+		case opt::BINOMIAL:
+			return log10(evalBinomialScore(rec, filter, subtract)) * -10;
+		case opt::SIMPLE:
+		default:
+			return evalSimpleScore(rec, filter, subtract);
+		}
 	}
 }
 }
