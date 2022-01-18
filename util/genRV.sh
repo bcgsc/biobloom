e531147d2d8060e0c03cb49f94e0c9d79129430e
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-18 23:58:45 +0800

    Delete util directory

--- util/genRV.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#Placeholder script for generating .rv (reversed files) needed for indexing a present time
-#Requires gnu awk, tac, tr, and seqtk to be in your path
-#usage genRV.sh filename.fa
-for filename; do
-	cat $filename | awk '/^>/ {printf("%s%s\t",(N>0?"\n":""),$0);N++;next;} {printf("%s",$0);} END {printf("\n");}' | tac | tr "\t" "\n" |seqtk seq -r - > $filename.rv
-	echo $filename".rv written"
-done
